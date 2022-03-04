217a320bcd87d801f43993f966090100e3b45f84
Sergej Herdt <sergej.herdt.sh@gmail.com>
2022-03-04 12:01:48 +0100

    Code additions and adjustments, which includes: - refactor fetchAddress to return value and throw exceptions directly instead of passing to two Consumers - add method to fetch Addresses asynchronously - add custom Exception implementation - extract TimeUnit parameter in CacheBuilder#expireAfterAccess to a private constant (+ comment/description) - remove unnecessary final modifier on parameters - adjust Javadocs to fit better/make more sense - rename method names to fit better/make more sense - rename AddressObject to AddressData - rename package to include project name

--- src/main/java/net/square/ProxyAPI.java
+++ src/main/java/net/square/ProxyAPI.java
@@ -8,111 +8,133 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.NonNull;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
-import net.square.objectable.AddressObject;
-import org.apache.commons.io.IOUtils;
+import net.square.address.AddressData;
+import net.square.exceptions.AddressDataFetchingException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 @UtilityClass
+@SuppressWarnings("unused")
 public class ProxyAPI {
+
+    // TODO: add short description
+    private static final String API_URL = "https://proxycheck.io/v2/%s?key=%s?vpn=1&asn=1";
 
     // If you have a plan on https://proxycheck.io, you should enter your key here. If you don't have one, you are
     // free to make 1000 requests per day for your own server/computer IP. I recommend buying a plan though.
     private static final String PROXY_KEY = "insert_key";
 
+    // How long should an entry be stored?
+    private static final int DURATION_TIME = 60;
+    // In which time unit should the duration time last?
+    private static final TimeUnit DURATION_UNIT = TimeUnit.MINUTES;
+
     // I couldn't think of a better way to turn the result from the website into a Java object without any problems.
     // Therefore, I have taken Gson.
     private final Gson gson = new Gson();
 
-    // How many minutes should an entry be stored? The time format can be changed at LoadingCache.
-    private static final int DURATION_TIME = 60;
-
     // To avoid unnecessary requests, I have implemented the Guava LoadingCache. This keeps an IP in the cache for
     // 60 minutes, after which it is removed and must be fetched again from the website when it is accessed again.
     // I recommend installing an own cache beside this one.
-    private final LoadingCache<String, AddressObject> cacheCat = CacheBuilder
-        .newBuilder()
-        .expireAfterAccess(DURATION_TIME, TimeUnit.MINUTES)
+    private final LoadingCache<String, AddressData> cacheCat = CacheBuilder.newBuilder()
+        .expireAfterAccess(DURATION_TIME, DURATION_UNIT)
         .build(CacheLoader.from(ProxyAPI::fetchData));
 
     /**
-     * Looks in the LoadingCache for the {@link AddressObject} of the passing IPv4. If this is not available, the cache
-     * fetches this object via the fetchData method. This whole process is asynchronous.
+     * Looks up an {@link AddressData} object for the passed IPv4 address inside the cache. If not cached, the cache
+     * attempts to fetch this object via {@link ProxyAPI#fetchData(String)}.
+     * <br>
+     * The cached entry will expire after every access after the given time
+     * (see {@link #DURATION_TIME} and {@link #DURATION_UNIT}).
+     * <br>
+     * Will throw if the cache fails during the fetch or when the fetched data returns null.
      *
-     * @param ipAddress The IPv4 as plain string
-     * @param address   Contains the {@link AddressObject} of the passed IPv4 address if available.
-     * @param ex        Contains the exception, if any, that may have occurred while processing the request.
+     * @param ipAddress A IPv4 address in string representation.
+     * @return An {@link AddressData} object for the given ipAddress
+     * @throws ExecutionException Thrown when cache returned null or when an exception was thrown inside the cache.
      */
-    public void getObjectFromIPv4(@NonNull final String ipAddress, final Consumer<AddressObject> address,
-                                  final Consumer<ExecutionException> ex) {
-
+    public AddressData fetchAddressDataForIPv4(@NonNull String ipAddress) throws ExecutionException {
         // Checks if the passed argument is null. There are some jokers :P
-        Preconditions.checkNotNull(ipAddress, "Field <address> cannot be null");
-
-        try {
-            address.accept(cacheCat.get(ipAddress));
-        } catch (ExecutionException e) {
-            ex.accept(e);
-        }
+        Preconditions.checkNotNull(ipAddress, "Field ipAddress cannot be null");
+        return cacheCat.get(ipAddress);
     }
 
     /**
-     * Formats the return from the https://proxycheck.io page as {@link AddressObject} and inserts it into the cache.
+     * Wraps the execution of {@link #fetchAddressDataForIPv4(String)} inside a {@link CompletableFuture} to retrieve
+     * its result asynchronously.
+     * <br>
      *
-     * @param address The IPv4 as plain string
-     * @return https://proxycheck.io result as {@link AddressObject}
-     * @throws NullPointerException If an error occurs while establishing the connection or processing the result.
+     * Thrown {@link Exception}s, like {@link AddressDataFetchingException}, have to be handled by using
+     * {@link CompletableFuture#exceptionally(Function)} or {@link CompletableFuture#whenComplete(BiConsumer)}.
+     *
+     * @param ipAddress A IPv4 address in string representation.
+     * @return The
      */
-    private AddressObject fetchData(@NonNull final String address) {
+    public CompletableFuture<AddressData> fetchAddressDataForIPv4Async(@NonNull String ipAddress) {
+        Preconditions.checkNotNull(ipAddress, "Field ipAddress cannot be null");
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return fetchAddressDataForIPv4(ipAddress);
+            } catch (ExecutionException e) {
+                throw new CompletionException(e);
+            }
+        });
+    }
 
-        JsonObject jsonObject = parseObjectFromURL(formatIPv4(address));
+    /**
+     * Converts the response body received by the HTTP GET request to the https://proxycheck.io API
+     * to an {@link AddressData} object.
+     *
+     * @param ipAddress A IPv4 address represented as a string.
+     * @return An {@link AddressData} object containing the information of the response body.
+     */
+    @SneakyThrows
+    private AddressData fetchData(@NonNull String ipAddress) {
+        Preconditions.checkNotNull(ipAddress, "Field ipAddress cannot be null");
+        JsonObject jsonObject;
+        try {
+            jsonObject = parseJsonObjectFromURL(formatURL(ipAddress));
+        } catch (IOException e) {
+            throw new AddressDataFetchingException("Failed to fetch data for address %s".formatted(ipAddress), e);
+        }
 
         Preconditions.checkNotNull(jsonObject);
         Preconditions.checkNotNull(jsonObject.get("status"));
         Preconditions.checkArgument(!jsonObject.get("status").getAsString().equalsIgnoreCase("ok"));
 
-        return gson.fromJson(getObjectFromObject(jsonObject, address), AddressObject.class);
+        return gson.fromJson(jsonObject.getAsJsonObject(ipAddress), AddressData.class);
     }
 
     /**
-     * Fetches an object from an object at a given field name.
+     * Converts the response ({@link URL#openStream()} of a URL to a {@link JsonObject}.
      *
-     * @param object The object from which you want to have something
-     * @param field  What the JsonObject inside the object is called and how to get it. Field name
-     * @return Returns the JsonObject from the JsonObject
+     * @param url The URL string from which to read and convert the response of.
+     * @return The object if it was successfully converted.
+     * @throws IOException Thrown when an error occurred while reading from the {@link java.io.InputStream} of the URL.
      */
-    private String getObjectFromObject(@NonNull final JsonObject object, @NonNull final String field) {
-        return object.get(field).getAsJsonObject().toString();
+    private JsonObject parseJsonObjectFromURL(@NonNull String url) throws IOException {
+        return JsonParser.parseReader(
+            new BufferedReader(new InputStreamReader(new URL(url).openStream()))).getAsJsonObject();
     }
 
     /**
-     * Converts the JsonObject behind a URL link to a JsonObject local.
+     * Formats the {@link ProxyAPI#API_URL} together with the IPv4 address and the API key.
      *
-     * @param url URL links from which the object should be fetched
-     * @return Returns the object if it was successfully converted.
+     * @param address The IPv4 address as plain string.
+     * @return A formatted string {@link ProxyAPI#API_URL}.
      */
-    private JsonObject parseObjectFromURL(@NonNull final String url) {
-        try {
-            return JsonParser.parseString(IOUtils.toString(new URL(url), StandardCharsets.UTF_8)).getAsJsonObject();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Formats the https://proxycheck.io domain together
-     * with the IPv4 and the API key.
-     *
-     * @param address The IPv4 as plain string
-     * @return Returns the https://proxycheck.io API URL formatted with the IPv4 and the API key.
-     */
-    private String formatIPv4(@NonNull final String address) {
-        return String.format("https://proxycheck.io/v2/%s?key=%s?vpn=1&asn=1", address, PROXY_KEY);
+    private String formatURL(@NonNull String address) {
+        return String.format(API_URL, address, PROXY_KEY);
     }
 }
---
+++ src/main/java/net/square/exceptions/AddressDataFetchingException.java
@@ -0,0 +1,7 @@
+package net.square.exceptions;
+
+public class AddressDataFetchingException extends Exception {
+    public AddressDataFetchingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
