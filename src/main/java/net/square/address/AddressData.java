fc7804e8a4ac07df88c22a606d3d09902b1d8c85
Felix <admin@squarecode.de>
2022-04-19 20:55:56 +0200

    commentary fix

--- src/main/java/net/square/address/AddressData.java
+++ src/main/java/net/square/address/AddressData.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 /**
  * This object contains information about one specific IPv4 address, which is retrieved by sending an HTTP GET request
- * to the API of https://proxycheck.io. It will represent the response body of the request that has been sent.
+ * to the API of <a href="https://proxycheck.io">https://proxycheck.io</a>.
+ * It will represent the response body of the request that has been sent.
  */
 @Data
 public class AddressData {
